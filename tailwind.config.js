0ef64da5d12e75818b0fd8f2176c8e96b25c6bbb
stefan-lemonworx <88316006+stefan-lemonworx@users.noreply.github.com>
2026-02-10 15:05:41 +0100

    Add Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",        // required by Vite
+    "./src/**/*.{ts,tsx}", // include all TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
